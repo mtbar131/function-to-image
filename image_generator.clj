f53d617c291dbd742cdafc6563ab909f59f311de
Amit Barve <mtbar131@gmail.com>
2016-06-01 17:10:58 +0530

    initial commit

---
+++ image_generator.clj
@@ -0,0 +1,59 @@
+(ns com.test
+  (:import (javax.swing JFrame))
+  (:gen-class))
+
+;; Different functions to generate pixel value
+; Each pixel has color value of xor of its x and y co-ordinates
+(defn generateXorList [limit]
+  (for [x (range limit)
+        y (range limit)]
+    [x y (bit-xor x y)]
+))
+
+; Each pixel has color value of addition of x & y
+(defn generateXmodYList [limit]
+  (for [x (range limit)
+        y (range limit)]
+    [x y (mod (+ x y) 256)] ; mod by 256 for rgb limit
+))
+
+; Each pixel has color value of bitwise ANDing of x  & y
+(defn generateXandYList [limit]
+  (for [x (range limit)
+        y (range limit)]
+    [x y (bit-and x y)] ; mod by 256 for rgb limit
+))
+
+;; JFrame setup and pixel coloring
+;;(def maxSize 250)
+
+;;(def frame (JFrame.))
+;; (.setVisible frame true)
+;; (.setSize frame maxSize maxSize)
+
+;;(def gfx (.getGraphics frame))
+
+;; (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+
+;; (defn colorPixel [[x y color] gfx]
+;;   (.setColor gfx (java.awt.Color. color))
+;;   (.fillRect gfx x y 1 1))
+
+(defn makeImage [seq]
+  (let [frameSize 250
+        frame (doto (JFrame.)
+                (.setVisible true)
+                (.setSize frameSize frameSize)
+                (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))
+        gfx (.getGraphics frame)]    
+    (doseq [[x y color] seq]      
+      (.setColor gfx (java.awt.Color. color))
+      (.fillRect gfx x y 1 1))))
+
+;; Generate the image
+(makeImage (generateXorList 250))
+;(makeImage (generateXmodYList 250))
+;(makeImage (generateXandYList 250))
+
+
+
